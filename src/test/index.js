e9c88dd0a67b71420297a352c5f12badffee3258
Jorge Lozano <jlozano@condorlabs.io>
2021-11-08 16:10:07 -0500

    Comitting src/test/index.js file

---
+++ src/test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+const app = require('./app')
+// Constants
+const PORT = 4000;
+const HOST = '0.0.0.0';
+
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+module.exports = app;
